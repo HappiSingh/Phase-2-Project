d0c92b92f6c3fad11afab2cb1f018efac474e245
Harpreet Singh <Harpreets1201@gmail.com>
2023-06-30 19:28:36 -0400

    completed triumph component

--- src/components/Triumphs.js
+++ src/components/Triumphs.js
@@ -1,19 +1,17 @@
-
 import React from "react";
 import TriumphItem from "./TriumphItem";
 
 function Triumphs({ triumphs, onDeleteClick }){
-
+// mapping and sending props data to child component 
     const renderTriumphs = triumphs.map(triumph => {
         return(
             <TriumphItem 
                 key={triumph.id} 
                 triumph={triumph}
                 onDeleteClick={onDeleteClick} 
-                />
+            />
         )
     })
-
 
     return(
         <div id="triumphs-container">
